c7dbbc140a0879fdbde0c48e49807b8a69c1fe70
liamack <liamack85@gmail.com>
2026-02-25 15:18:09 -0500

    progress so far

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,25 @@
--- TODO
+DROP TABLE IF EXISTS playlist_tracks;
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS tracks;
+
+
+CREATE TABLE playlists (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  description text NOT NULL
+);
+
+CREATE TABLE tracks (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  duration_ms int NOT NULL
+);
+
+CREATE TABLE playlist_tracks (
+  id serial PRIMARY KEY,
+  playlist_id int NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+  track_id int NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
+  UNIQUE (playlist_id, track_id)
+);
+
+--CREATE junction table AFTER primary tables
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,31 @@
 import db from "#db/client";
+import { createTrack } from "./queries/tracks.js";
+import { createPlaylist } from "./queries/playlists.js";
+import { createPlaylistTrack } from "./queries/playlistTracks.js";
 
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seed() {
-  // TODO
+async function seed(
+  NUM_TRACKS = 20,
+  NUM_PLAYLISTS = 10,
+  NUM_PLAYLIST_TRACKS = 15,
+) {
+  for (let i = 0; i < NUM_TRACKS; i++) {
+    const name = "Track " + i;
+    const durationMs = 1000000;
+    await createTrack(name, durationMs);
+  }
+  for (let i = 0; i < NUM_PLAYLISTS; i++) {
+    const name = "Playlist " + i;
+    const description = "Description " + i;
+    await createPlaylist(name, description);
+  }
+  for (let i = 0; i < NUM_PLAYLIST_TRACKS; i++) {
+    const playlistId = 1 + Math.floor(i / 3);
+    const trackId = 1 + i;
+    await createPlaylistTrack(playlistId, trackId);
+  }
 }
