0b2e9d577ca71b9d38c5d9db04f559413133c9ed
liamack <liamack85@gmail.com>
2026-02-26 09:51:48 -0500

    updated

--- db/queries/playlistTracks.js
+++ db/queries/playlistTracks.js
@@ -2,7 +2,7 @@ import db from "#db/client";
 
 export async function createPlaylistTrack(playlistId, trackId) {
   const sql = `
-  INSERT INTO playlists_tracks
+  INSERT INTO playlist_tracks
     (playlist_id, track_id)
   VALUES
     ($1, $2)
